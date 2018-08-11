2fcf08f562258160d3c86630d5dcfcf2280b88e1
paiakshay998 <paiakshay998@gmail.com>
2018-08-11 22:22:32 +0530

    Add contributor code

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,5 @@
 import EmbarkJS from 'Embark/EmbarkJS';
-// import Region from 'Embark/contracts/Region';
+import Region from 'Embark/contracts/Region';
 // import your contracts
 // e.g if you have a contract named SimpleStorage:
 //import SimpleStorage from 'Embark/contracts/SimpleStorage';
@@ -48,6 +48,32 @@ function getRegionId(latitude, longitude) {
     return regionId;
 }
 
+function pushDataToIPFS(dataAggregate) {
+    const ipfs = IpfsApi('localhost', 5001);
+    let json = { "data": [] };
+    for (var i=0; i<dataAggregate.length; i++) {
+        let data = {
+            startRegion: dataAggregate[i][0],
+            contribStartPosLatitude: dataAggregate[i][1],
+            contribStartPosLongitude: dataAggregate[i][2],
+            contribEndPosLatitude: dataAggregate[i][3],
+            contribEndPosLongitude: dataAggregate[i][4],
+        };
+        json['data'].push(data);
+    }
+    console.log(json);
+    let hash = '';
+    ipfs.files.add(Buffer.from(JSON.stringify(json)), (err, result) => {
+        if(err) {
+          console.error(err)
+          return
+        }
+        hash = result[0].hash;
+        console.log(hash, result[0].hash);
+    });
+    return hash;
+}
+
 window.onload = function() {
     var currentLatitude=0, currentLongitude=0;
     var currentRegion = -1;
@@ -76,4 +102,35 @@ window.onload = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawMap(ctx, data, currentLatitude, currentLongitude, currentRegion);
     }, 3000);
+
+    // CONTRIBUTOR CODE STARTS HERE
+        var contribStartPosLatitude, contribStartPosLongitude;
+        navigator.geolocation.getCurrentPosition(function showPosition(position) {
+            contribStartPosLatitude = position.coords.latitude;
+            contribStartPosLongitude = position.coords.longitude;
+        });
+        var dataAggregate = []; 
+        var count = 0;
+        setInterval(function() {
+            var contribEndPosLatitude, contribEndPosLongitude;
+            navigator.geolocation.getCurrentPosition(function showPosition(position) {
+                contribEndPosLatitude = position.coords.latitude;
+                contribEndPosLongitude = position.coords.longitude;
+            });
+            // if he has changed his location
+            if (! (contribEndPosLatitude == contribStartPosLatitude && contribEndPosLongitude == contribStartPosLongitude)) {
+                // if he travels two regions, then he gets contribution to both regions
+                var startRegion = getRegionId(contribStartPosLatitude, contribStartPosLongitude);
+                dataAggregate.push([startRegion, contribStartPosLatitude, contribStartPosLongitude, contribEndPosLatitude, contribEndPosLongitude]);
+            }
+            count += 5;
+            console.log(count);
+            // after aggregating for five minutes, push to ipfs
+            if (count >= 20) {
+                pushDataToIPFS(dataAggregate);
+                dataAggregate = [];
+                count = 0;
+            }
+        }, 5000);
+    // CONTRIBUTOR CODE ENDS HERE
 }
